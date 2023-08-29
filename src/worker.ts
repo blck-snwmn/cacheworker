aa53a285148318efb86988942afa79b1de8119fd
blck-snwmn <whitesnowmancreator@gmail.com>
2023-08-30 02:06:39 +0900

    Use cache

--- src/worker.ts
+++ src/worker.ts
@@ -5,15 +5,27 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const object = await env.CACHE_BUCKET.get('key-id-xxxxxx.png');
+		const url = new URL(request.url);
+
+		const r2Key = 'key-id-xxxxxx.png'
+		const cacheKey = `${url.origin}/${r2Key}`
+
+		const cache = caches.default;
+		const cacheObj = await cache.match(cacheKey)
+		if (cacheObj) {
+			return cacheObj
+		}
+		const object = await env.CACHE_BUCKET.get(r2Key);
 		if (object === null) {
 			return new Response('Not Found', { status: 404 });
 		}
-		return new Response(object.body, {
+		const resp = new Response(object.body, {
 			status: 200,
 			headers: {
 				'Content-Type': object.httpMetadata?.contentType ?? "application/octet-stream"
 			}
 		});
+		await cache.put(cacheKey, resp.clone())
+		return resp
 	},
 };
